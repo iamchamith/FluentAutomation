2d2ca9090b232a7a9e97fe5e932ceb16c2f83164
Chamith Saranga <chamith@inexisconsulting.com>
2020-05-12 13:15:26 +0530

    enhancement

---
+++ src/ICrudListBasePage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+namespace FluentAutomation
+{
+    public interface ICrudListBasePage<Me>
+    {
+        IWebElement Filter { get; set; }
+        IWebElement CreateNew { get; set; }
+        Me Bind();
+        Me ViewInfoByIndex(int index = 0);
+        Me DoFilter(string query);
+        Me DoCreateNew();
+    }
+}
---
+++ src/ICrudManageBasePage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace FluentAutomation
+{
+    public interface ICrudManageBasePage<ViewModel, Me> where ViewModel : class
+    {
+        ViewModel _itemViewModel { get; set; }
+        IWebElement Save { get; set; }
+        IWebElement Delete { get; set; }
+        IWebElement ToList { get; set; }
+        Me Bind();
+        Me SetValue(ViewModel item);
+        Me SetUpdateValue(ViewModel item);
+        Me DoDelete();
+        Me DoSave();
+        Me GoToList();
+        Me DoAssert(ViewModel item = null);
+    }
+}
