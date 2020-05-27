b863572819d5547f6912493f5557d11668207b43
Chamith Saranga <chamith@inexisconsulting.com>
2020-05-27 15:19:17 +0530

    https://inexis.atlassian.net/browse/MP-2212

--- src/AutomationTestConfig.cs
+++ src/AutomationTestConfig.cs
@@ -12,7 +12,7 @@ namespace FluentAutomation
         public static string WebDriverLocation = @"C:\webdrivers";
 
         public static string DbConnectionString = "Server=52.148.93.180; Port=3306; Database=expresslk_qa; Uid=bbk; Pwd=hard2crack2009$;SslMode=none;SslMode=Preferred;";
-        public static string Host { get; set; } = "http://admin.carwashcmstest.com/login";
+        public static string Host { get; set; } = "http://gardaeialtd.test.assocify.io/#/login";
 
         public static string ApiDomain { get; set; } = "https://test-api.express.lk/";
         public static AutomationEnvironment Env { get; set; } = new AutomationEnvironment(new BrowserConfig(AutomationEnums.Browser.Chrome, Device.Laptop, Resalution.None));
